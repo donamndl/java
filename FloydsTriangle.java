05dfb7b6c565b5d3bb6287add3602a451d5bc32a
dona125 <donamndl125@gmail.com>
2026-05-12 12:20:47 +0530

    Concept of Floyds Triangle pattern

---
+++ FloydsTriangle.java
@@ -0,0 +1,22 @@
+package com.java.Practice;
+import java.util.*;
+
+public class FloydsTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int n = sc.nextInt();
+		int count = 1;
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(count + " ");
+				count++;
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+
+}
